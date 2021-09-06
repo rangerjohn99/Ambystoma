db4a65e61bedb8753a202ed960d7eb6d5dc2b111
David Ledesma <ledesma-david@utexas.edu>
2021-09-06 16:47:27 -0500

    cc

--- Ambystoma.R
+++ Ambystoma.R
@@ -1,13 +1,28 @@
+## Load in data ##
+# Read in csv file of data---------------------------------
+library(curl)
+f2 <- curl("https://raw.githubusercontent.com/rangerjohn99/Ambystoma/main/Ambystoma_final.csv")
+Ambystoma_final <- read.csv(f2, header = TRUE, sep = ",", stringsAsFactors = TRUE) # this is a matrix of each specimen  
+head(Ambystoma_final)
+
+
 Ambystoma_final$larval <- as.factor(Ambystoma_final$larval)
 Ambystoma_final$atlas_fused <- as.factor(Ambystoma_final$atlas_fused) 
 Ambystoma_final$offset <- as.factor(Ambystoma_final$offset)
 
-gsub("_UTEP.*","", Ambystoma_final$specimen)
+Ambystoma_final$Sex[Ambystoma_final$Sex=='']=NA
+Ambystoma_final$Sex = droplevels(Ambystoma_final$Sex)
+str(Ambystoma_final)
+
+Ambystoma_final$species <-gsub("_UTEP.*","", Ambystoma_final$specimen)
 
 library(car)
+library(lme4)
 
 # need to deal with getting a species variable
 # not sure how to deal with NA and missing values (why does sex not have NA values?)
+model <- lmer(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final)
+plot(check_distribution(model))
 
 myaov <- aov(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final,contrasts=list(larval=contr.sum,Sex=contr.sum,offset=contr.sum))  
 
