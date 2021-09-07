c10e8a5b41fbea7876b2eab5f5f3e43b767fbfb6
David Ledesma <ledesma-david@utexas.edu>
2021-09-07 12:23:33 -0500

    cc

--- Ambystoma.R
+++ Ambystoma.R
@@ -21,18 +21,58 @@ Ambystoma_final$species <-gsub("_UTEP.*","", Ambystoma_final$specimen) #makes sp
 library(car)
 library(performance)
 library(olsrr)
+library(afex)
 
+# ANOVA ---------------------------------
+# Check model assumptions
 hist(Ambystoma_final$trunk, main="Histogram of Trunk Count", xlab="Number of Trunk verts")  
-
-model <- aov(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final)
-check_model(model)
 
 model <- lm(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final)
 ols_test_normality(model)
 plot(model)
+is_norm <- check_normality(model)
+plot(is_norm)
+plot(is_norm, type = "qq")
+plot(is_norm, type = "qq", detrend = TRUE)
+
+model <- aov(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final)
+check_model(model)
 
 myaov <- aov(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final,contrasts=list(larval=contr.sum,Sex=contr.sum,offset=contr.sum))  
 
 Anova(myaov, type=3)
 
 summary.lm(myaov)$adj.r.squared
+
+# Ordinal Logistic Regression---------------------------------
+library(MASS)
+#Ordering the dependent variable
+Ambystoma_final$trunk = factor(Ambystoma_final$trunk, levels = c("12", "13", "14", "15", "16", "17"), ordered = TRUE) 
+Ambystoma_final$caudosacral = factor(Ambystoma_final$caudosacral, levels = c("1", "2", "3"), ordered = TRUE) 
+
+#Exploratory data analysis
+#Summarizing the data
+  summary(Ambystoma_final)
+library(ggplot2)
+ggplot(Ambystoma_final, aes(x = trunk, y = SVL_P, fill = Sex)) +   geom_boxplot(size = .75) +   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+# Making the models
+model_fit <- polr(trunk~log(SVL_P)+larval+Sex+offset,data=Ambystoma_final, Hess = TRUE)
+summary(model_fit)
+
+model_fit_nosex <- polr(trunk~log(SVL_P)+larval+offset,data=Ambystoma_final, Hess = TRUE)
+summary(model_fit_nosex)
+
+# Comparing models
+library(AICcmodavg)
+
+models <- list(model_fit, model_fit_nosex)
+model.names <- c('model_fit', 'model_fit_nosex')
+aictab(cand.set = models, modnames = model.names)
+
+# Significance of coefficients and intercepts
+summary_table <- coef(summary(model_fit))
+pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
+summary_table <- cbind(summary_table, "p value" = round(pval,3))
+summary_table
+
