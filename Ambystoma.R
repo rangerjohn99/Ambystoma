7b8504837e431e3df33ffaf2435dc4bc99d239cc
rangerjohn99 <65524029+rangerjohn99@users.noreply.github.com>
2021-09-16 21:01:27 -0500

    y

--- Ambystoma.R
+++ Ambystoma.R
@@ -53,7 +53,7 @@ Ambystoma_final$caudosacral = factor(Ambystoma_final$caudosacral, levels = c("1"
 
 #Exploratory data analysis
 #Summarizing the data
-  summary(Ambystoma_final)
+summary(Ambystoma_final)
 library(ggplot2)
 ggplot(Ambystoma_final, aes(x = trunk, y = SVL_P, fill = Sex)) +facet_wrap(. ~ species, ncol = 3)+   geom_dotplot(binaxis = "y",stackdir = "center") +   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
@@ -77,3 +77,18 @@ pval <- pnorm(abs(summary_table[, "t value"]),lower.tail = FALSE)* 2
 summary_table <- cbind(summary_table, "p value" = round(pval,3))
 summary_table
 
+# centrum model
+
+Ambystoma_final$centrum_trunk <- as.factor(Ambystoma_final$centrum_trunk)
+Ambystoma_final$centrum_sacral <- as.factor(Ambystoma_final$centrum_sacral) 
+Ambystoma_final$larval <- as.factor(Ambystoma_final$larval)
+
+# 8th centrum model
+
+centrum_model <- lm(centrum_trunk~log(SVL_P)+larval+Sex+species,data=Ambystoma_final)
+ols_test_normality(centrum_model)
+plot(centrum_model)
+iscentrum8_norm <- check_normality(centrum_model)
+plot(iscentrum8_norm)
+plot(iscentrum8_norm, type = "qq")
+plot(iscentrum8_norm, type = "qq", detrend = TRUE)
